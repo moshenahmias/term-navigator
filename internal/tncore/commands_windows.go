ea9b07c3a291ed02333e8e0d030f100097e6876f
Moshe Nahmias <drlmbr@gmail.com>
2026-04-17 09:58:28 +0300

    Update commands_windows.go

--- internal/tncore/commands_windows.go
+++ internal/tncore/commands_windows.go
@@ -5,7 +5,6 @@ package tncore
 import (
 	"os"
 	"os/exec"
-	"strings"
 
 	tea "charm.land/bubbletea/v2"
 	"github.com/moshenahmias/term-navigator/internal/backends/local"
@@ -16,11 +15,17 @@ func init() {
 		if len(args) != 0 {
 			return failure("Usage: logs")
 		}
+		tmp, _ := os.CreateTemp("", "logs-*.txt")
+		tmp.WriteString(a.logBuffer.String())
+		tmp.Close()
 
-		cmd := exec.Command("notepad")
-		cmd.Stdin = strings.NewReader(a.logBuffer.String())
+		cmd := exec.Command("notepad.exe", tmp.Name())
 
-		return tea.ExecProcess(cmd, execCheck())
+		return tea.ExecProcess(cmd, func(err error) tea.Msg {
+			// Delete immediately after launching
+			os.Remove(tmp.Name())
+			return execCheck()(err)
+		})
 	}}
 
 	commands["cmd"] = command{f: func(a *App, args ...string) tea.Cmd {
